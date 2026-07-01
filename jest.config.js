4afcc9787b4f1b1f012f706880b092198081fc12
hbowo430-sudo <hbowo430@gmail.com>
2026-07-01 12:11:58 +0700

    Add Jest configuration for comprehensive testing

---
+++ jest.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@core/(.*)$': '<rootDir>/src/core/$1',
+    '^@gesture/(.*)$': '<rootDir>/src/gesture/$1',
+    '^@builder/(.*)$': '<rootDir>/src/builder/$1',
+    '^@wasm/(.*)$': '<rootDir>/src/wasm/$1',
+    '^@performance/(.*)$': '<rootDir>/src/performance/$1',
+    '^@encryption/(.*)$': '<rootDir>/src/encryption/$1',
+    '^@export-import/(.*)$': '<rootDir>/src/export-import/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/index.ts',
+    '!src/**/index.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: {
+        esModuleInterop: true,
+        allowSyntheticDefaultImports: true,
+      },
+    },
+  },
+};
